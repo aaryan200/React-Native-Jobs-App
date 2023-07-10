844956e8c91d41e53d55934daca2d87a2b7dc227
aaryan200 <co21btech11001@iith.ac.in>
2023-07-10 20:03:21 +0530

    First commit

---
+++ utils/index.js
@@ -0,0 +1,8 @@
+// checks if the url contains an image or not
+export const checkImageURL = (url) => {
+    if (!url) return false
+    else {
+        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');
+        return pattern.test(url);
+    }
+};
